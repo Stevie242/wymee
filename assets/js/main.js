eff7887a72b565b81d6295e48ffe15281d7c8db3
unknown <stevietathy@gmail.com>
2024-01-20 19:47:42 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var menuToggle = document.getElementById('toggle-menu');
-    var menu = document.querySelector('.header .menu-nav');
-    var closeMenu = document.getElementById('close-menu');
+  var menuToggle = document.getElementById('toggle-menu');
+  var menu = document.querySelector('.header .menu-nav');
+  var closeMenu = document.getElementById('close-menu');
 
-    menuToggle.addEventListener('click', function (event) {
-      // Inverser l'état du menu (ouvrir/fermer)
-      event.stopPropagation();
-      menu.classList.add('open');
-    });
-
-    closeMenu.addEventListener('click', function (event) {
-      // Inverser l'état du menu (ouvrir/fermer)
-      event.stopPropagation();
-      menu.classList.remove('open');
-    });
-  
-    // Fermer le menu lorsqu'on clique en dehors de la zone du menu
-    document.addEventListener('click', function (event) {
-       var menuContent = document.querySelector('.menu-nav-content');
-      var isClickInsideMenu = menuContent.contains(event.target);
-      var isClickOnToggle = event.target === menuToggle;
-  
-      if (!isClickInsideMenu && !isClickOnToggle) {
-        menu.classList.remove('open');
-      }
-    });
+  menuToggle.addEventListener('click', function (event) {
+    // Inverser l'état du menu (ouvrir/fermer)
+    event.stopPropagation();
+    menu.classList.add('open');
   });
-  
+
+  closeMenu.addEventListener('click', function (event) {
+    // Inverser l'état du menu (ouvrir/fermer)
+    event.stopPropagation();
+    menu.classList.remove('open');
+  });
+
+  // Fermer le menu lorsqu'on clique en dehors de la zone du menu
+  document.addEventListener('click', function (event) {
+    var menuContent = document.querySelector('.menu-nav-content');
+    var isClickInsideMenu = menuContent.contains(event.target);
+    var isClickOnToggle = event.target === menuToggle;
+
+    if (!isClickInsideMenu && !isClickOnToggle) {
+      menu.classList.remove('open');
+    }
+  });
+});
