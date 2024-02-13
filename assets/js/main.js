28cceac86e1d542614e35320e72067de18b97eb0
unknown <stevietathy@gmail.com>
2024-02-13 12:19:42 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,69 @@ document.addEventListener('scroll', function() {
     navbar.classList.remove('sticky');
   }
 });
+
+const dragContainer = document.getElementById('drag-container');
+
+if (dragContainer != null) {
+  const draggableElement = document.getElementById('draggable-element');
+  const fileInput = document.getElementById('fileInputCv');
+  const myForm = document.getElementById('matchingForm');
+
+  dragContainer.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dragContainer.classList.add('drag-over');
+    const fileNameText = document.getElementById('file-name-text');
+    fileNameText.textContent = 'Déposer votre Cv';
+  });
+
+  dragContainer.addEventListener('dragleave', () => {
+    dragContainer.classList.remove('drag-over');
+    const fileNameText = document.getElementById('file-name-text');
+    fileNameText.textContent = 'Ajoutez votre cv on s\'occupe du reste';
+  });
+
+  dragContainer.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dragContainer.classList.remove('drag-over');
+    const files = e.dataTransfer.files || fileInput.files;
+    handleFiles(files);
+  });
+
+  fileInput.addEventListener('change', (e) => {
+    const files = e.target.files;
+    handleFiles(files);
+  });
+
+  myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Ajoutez ici la logique de soumission du formulaire si nécessaire
+    alert('Form submitted!');
+  });
+
+  function handleFiles(files) {
+    const spinner = document.getElementById('spinner');
+    const fileNameText = document.getElementById('file-name-text');
+  
+    if (files.length > 0) {
+      const file = files[0];
+      if (file.type === 'application/pdf') {
+        const fileName = file.name;
+  
+        // Affiche le spinner pendant le traitement
+        spinner.style.display = 'inline-block';
+        fileNameText.style.display = 'none';
+  
+        // Simulation d'un délai (remplacez cela par votre logique réelle)
+        setTimeout(() => {
+          // Affiche le nom du fichier et cache le spinner une fois le traitement terminé
+          fileNameText.textContent = `PDF File: ${fileName}`;
+          spinner.style.display = 'none';
+          fileNameText.style.display = 'inline-block';
+        }, 2000); // Remplacez 2000 par la durée réelle de votre traitement
+      } else {
+        alert('Veuillez sélectionner un fichier PDF.');
+      }
+    }
+  }
+  
+}
