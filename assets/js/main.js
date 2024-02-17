2ba5556b78f988c343d9cb5a9504a61564326f1b
unknown <stevietathy@gmail.com>
2024-02-17 19:29:46 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,11 +69,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('scroll', function () {
   var navbar = document.getElementById('topBar');
-
-  if (window.scrollY > 100) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
+  if (navbar != null) {
+    if (window.scrollY > 100) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
   }
 });
 
@@ -136,9 +137,11 @@ if (dragContainer != null) {
           fileNameText.style.display = 'inline-block';
         }, 2000); // La durée réelle de votre traitement
         //Redirection vers la page matching
-        setTimeout(() => {
-          myForm.submit();
-        }, 3000);
+        if (myForm != null) {
+          setTimeout(() => {
+            myForm.submit();
+          }, 3000);
+        }
       } else {
         alert('Veuillez sélectionner un fichier PDF.');
       }
@@ -147,7 +150,267 @@ if (dragContainer != null) {
 
 }
 
+/* *Register page* */
+
+const skillAddBtn = document.getElementById('addSkill');
+
+if (skillAddBtn != null) {
+  const listSkills = document.getElementById('skillList');
+  const skillText = document.getElementById('skillText');
+
+  skillAddBtn.addEventListener('click', function () {
+    const taskText = skillText.value.trim();
+
+    if (taskText !== '') {
+      addSkillTask(taskText);
+      skillText.value = '';
+    }
+  });
+
+
+  skillText.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      const taskSkillText = skillText.value.trim();
+
+      if (taskSkillText !== '') {
+        addSkillTask(taskSkillText);
+        skillText.value = '';
+      }
+    }
+  });
+
+  function addSkillTask(text) {
+    const taskSkillItem = document.createElement('div');
+    taskSkillItem.className = 'skill-detail';
+    taskSkillItem.innerHTML = `
+    <input type="text" class="d-none" value="${text}" name="skill">
+    <span>${text}</span>
+    <button class="deleteSkill" type="button"><i class="fa-solid fa-xmark"></i>
+    `;
+
+    // Ajout de l'écouteur d'événement click au nouveau bouton de suppression
+    const newDeleteSkillTaskButton = taskSkillItem.querySelector('.deleteSkill');
+    newDeleteSkillTaskButton.addEventListener('click', () => {
+      const taskElement = newDeleteSkillTaskButton.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+
+    listSkills.appendChild(taskSkillItem);
+  }
+
+  var deleteSkillTaskButton = document.querySelectorAll('.deleteSkill');
+
+  deleteSkillTaskButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      const taskElement = button.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+  });
+
+  const delSkill = document.getElementById('delSkill');
+  delSkill.addEventListener('click', function () {
+    skillText.value = '';
+  })
+}
+
+const xpAddBtn = document.getElementById('add-xp');
+
+if (xpAddBtn != null) {
+  const listXps = document.getElementById('list-xp');
+  const xpTitle = document.getElementById('title-add');
+  const xpSociaty = document.getElementById('sociaty-add');
+  const xpAddress = document.getElementById('address-add');
+  const xpdescription = document.getElementById('description-add');
+  const xpDateStart = document.getElementById('date-start-add');
+  const xpDateEnd = document.getElementById('date-end-add');
+
+  xpAddBtn.addEventListener('click', function () {
+    const xpTitleText = xpTitle.value.trim();
+    const xpSociatyText = xpSociaty.value.trim();
+    const xpAddressText = xpAddress.value.trim();
+    const xpdescriptionText = xpdescription.value.trim();
+    const xpDateStartText = xpDateStart.value.trim();
+    const xpDateEndText = xpDateEnd.value.trim();
+
+    if (xpTitleText !== '' || xpSociatyText !== '' || xpAddressText !== '' || xpdescriptionText !== '' || xpDateStartText !== '' || xpDateEndText !== '') {
+      addXpTask(xpTitleText, xpSociatyText, xpAddressText, xpdescriptionText, xpDateStartText, xpDateEndText);
+      xpTitle.value = '';
+      xpSociaty.value = '';
+      xpAddress.value = '';
+      xpdescription.value = '';
+      xpDateStart.value = '';
+      xpDateEnd.value = '';
+    }
+  });
+
+  function addXpTask(xpTitle, xpSociaty, xpAddress, xpdescription, xpDateStart, xpDateEnd) {
+    const taskXpItem = document.createElement('div');
+    taskXpItem.className = 'experience-detail';
+    taskXpItem.innerHTML = `
+    <div class="group-input">
+        <div class="input-form">
+            <label>${xpTitle}</label>
+            <label>${xpDateStart} - ${xpDateEnd}</label>
+            <input type="text"name="title" value="${xpTitle}">
+            <input type="text" name="date" value="${xpDateStart} - ${xpDateEnd}}">
+        </div>
+        <div class="input-form">
+            <label>${xpSociaty}</label>
+            <label>${xpAddress}</label>
+            <input type="text" name="sociaty" value="${xpSociaty}">
+            <input type="text" name="address" value="${xpAddress}">
+        </div>
+        <div class="input-form">
+            <span>${xpdescription}</span>
+            <textarea name="description" value="${xpdescription}"></textarea>
+        </div>
+    </div>
+    <div class="action">
+        <button type="button">
+            <i class="fa-solid fa-ellipsis"></i>
+        </button>
+        <div class="menu-action"></div>
+    </div>
+    `;
+
+    // Ajout de l'écouteur d'événement click au nouveau bouton de suppression
+    /*
+    const newDeleteSkillTaskButton = taskXpItem.querySelector('.deleteSkill');
+    newDeleteSkillTaskButton.addEventListener('click', () => {
+      const taskElement = newDeleteSkillTaskButton.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+    */
+    listXps.appendChild(taskXpItem);
+  }
+  /*
+  var deleteSkillTaskButton = document.querySelectorAll('.deleteSkill');
+
+  deleteSkillTaskButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      const taskElement = button.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+  });
+  */
+  const delXp = document.getElementById('del-xp');
+  delXp.addEventListener('click', function () {
+    xpTitle.value = '';
+    xpSociaty.value = '';
+    xpAddress.value = '';
+    xpdescription.value = '';
+    xpDateStart.value = '';
+    xpDateEnd.value = '';
+  })
+}
+
+
+const trainingAddBtn = document.getElementById('addTraining');
+
+if (trainingAddBtn != null) {
+  const listTraining = document.getElementById('list-training');
+  const nameSchool = document.getElementById('school-add');
+  const trainingSpeciality = document.getElementById('speciality-add');
+  const trainingLevel = document.getElementById('level-add');
+  const trainingAddress = document.getElementById('training-address-add');
+  const trainingdescription = document.getElementById('description-b-add');
+  const trainingDateStart = document.getElementById('training-date-start-add');
+  const trainingDateEnd = document.getElementById('training-date-end-add');
+
+  trainingAddBtn.addEventListener('click', function () {
+    const nameSchoolText = nameSchool.value.trim();
+    const trainingSpecialityText = trainingSpeciality.value.trim();
+    const trainingLevelText = trainingLevel.value.trim();
+    const trainingAddressText = trainingAddress.value.trim();
+    const trainingdescriptionText = trainingdescription.value.trim();
+    const trainingDateStartText = trainingDateStart.value.trim();
+    const trainingDateEndText = trainingDateEnd.value.trim();    
+    console.log([nameSchoolText,
+      trainingAddressText,
+      trainingLevelText,
+      trainingAddressText,
+      trainingdescriptionText,
+      trainingDateStartText,
+      trainingDateEndText
+    ])
+    if (nameSchoolText !== '' || trainingSpecialityText !== '' || trainingLevelText !== '' || trainingAddressText !== '' || trainingdescriptionText !== '' || trainingDateStartText !== '' || trainingDateEndText !== '') {
+      addTrainingTask(nameSchoolText, trainingSpecialityText, trainingLevelText, trainingAddressText, trainingdescriptionText, trainingDateStartText, trainingDateEndText);
+      nameSchool.value = '';
+      trainingSpeciality.value = '';
+      trainingLevel.value = '';
+      trainingAddress.value = '';
+      trainingdescription.value = '';
+      trainingDateStart.value = '';
+      trainingDateEnd.value = '';
+    }
+  });
+
+
+  function addTrainingTask(nameSchoolText, trainingSpecialityText, trainingLevelText, trainingAddressText, trainingdescriptionText, trainingDateStartText, trainingDateEndText) {
+    const taskTrainingItem = document.createElement('div');
+    taskTrainingItem.className = 'training-detail';
+    taskTrainingItem.innerHTML = `
+    <div class="group-input">
+      <div class="input-form">
+          <label>${nameSchoolText}</label>
+          <label>${trainingDateStartText} - ${trainingDateEndText}</label>
+          <input type="text"name="school" value="${nameSchoolText}">
+          <input type="text" name="date" value="${trainingDateStartText} - ${trainingDateEndText}">
+      </div>
+      <div class="input-form">
+          <label>${trainingSpecialityText}</label>
+          <label>${trainingLevelText}</label>
+          <input type="text" name="speciality" value="${trainingSpecialityText}">
+          <input type="text" name="level" value="${trainingLevelText}">
+      </div>
+      <div class="input-form">
+          <label class="none">${trainingAddressText}</label>
+          <input type="text" name="address" value="${trainingAddressText}">
+      </div>
+      <div class="input-form">
+          <span>${trainingdescriptionText}</span>
+          <textarea name="description-b" value="${trainingdescriptionText}"></textarea>
+      </div>
+    </div>
+    <div class="action">
+      <button type="button">
+        <i class="fa-solid fa-ellipsis"></i>
+      </button>
+      <div class="menu-action"></div>
+    </div>
+    `;
+
+    // Ajout de l'écouteur d'événement click au nouveau bouton de suppression
+    /*
+    const newDeleteSkillTaskButton = taskTrainingItem.querySelector('.deleteSkill');
+    newDeleteSkillTaskButton.addEventListener('click', () => {
+      const taskElement = newDeleteSkillTaskButton.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+    */
+    listTraining.appendChild(taskTrainingItem);
+  }
+  /*
+  var deleteTrainingTaskButton = document.querySelectorAll('.deleteSkill');
+
+  deleteSkillTaskButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      const taskElement = button.parentNode;
+      taskElement.parentNode.removeChild(taskElement);
+    });
+  });
+  */
+  const delTraining = document.getElementById('delTraining');
+  delSkill.addEventListener('click', function () {
+    skillText.value = '';
+  })
+}
+
+
+/* *Register page* */
+
 document.addEventListener('DOMContentLoaded', function () {
+
   const addSkillBtn = document.getElementById('addSkillBtn');
 
   if (addSkillBtn != null) {
@@ -333,29 +596,29 @@ if (zSpinner != null) {
 
 
 const loader = document.getElementById('loader');
-if (loader !=null) {
+if (loader != null) {
   const progressBar = document.getElementById('progress-bar');
-        const span = document.querySelector('#progress-bar span');
+  const span = document.querySelector('#progress-bar span');
 
-        let width = 0;
-        let color = '#000';
-        const interval = setInterval(() => {
-            width += 1;
-            span.style.width = `${width}%`;
+  let width = 0;
+  let color = '#000';
+  const interval = setInterval(() => {
+    width += 1;
+    span.style.width = `${width}%`;
 
-            if (width <= 33) {
-                color = '#000';
-            } else if (width <= 66) {
-                color = '#F0733B';
-            } else {
-                color = '#E32481';
-            }
-            
-            span.style.backgroundColor = color;
+    if (width <= 33) {
+      color = '#000';
+    } else if (width <= 66) {
+      color = '#F0733B';
+    } else {
+      color = '#E32481';
+    }
 
-            if (width === 115) {
-                clearInterval(interval);
-                loader.style.display = 'none';
-            }
-        }, 15);
+    span.style.backgroundColor = color;
+
+    if (width === 115) {
+      clearInterval(interval);
+      loader.style.display = 'none';
+    }
+  }, 15);
 }
